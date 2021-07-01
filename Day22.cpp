3c5c8d7d9c078db0c4f8e3957c2d191f41d84a09
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-01 17:22:01 +0530

    Update Day22.cpp

--- Day22.cpp
+++ Day22.cpp
@@ -1,26 +1,58 @@
-class Node:
-    def __init__(self,data):
-        self.right=self.left=None
-        self.data = data
-class Solution:
-    def insert(self,root,data):
-        if root==None:
-            return Node(data)
-        else:
-            if data<=root.data:
-                cur=self.insert(root.left,data)
-                root.left=cur
-            else:
-                cur=self.insert(root.right,data)
-                root.right=cur
-        return root
-    def getHeight(self, root):
-        return -1 if root is None else 1 + max(self.getHeight(root.left), self.getHeight(root.right))
-T=int(input())
-myTree=Solution()
-root=None
-for i in range(T):
-    data=int(input())
-    root=myTree.insert(root,data)
-height=myTree.getHeight(root)
-print(height)
+#include <iostream>
+#include <cstddef>
+using namespace std;    
+class Node{
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d){
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+class Solution{
+    public:
+          Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            }
+            else {
+                Node* cur;
+                if(data <= root->data){
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                }
+                else{
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+               return root;
+           }
+        }
+        int getHeight(Node* root){
+          //Write your code here
+          if(!root) {
+              return -1;
+          }
+          int leftDepth = getHeight(root->left);
+          int rightDepth = getHeight(root->right);
+              
+          return (leftDepth > rightDepth ? leftDepth : rightDepth) + 1;
+        }
+}; //End of Solution
+int main() {
+    Solution myTree;
+    Node* root = NULL;
+    int t;
+    int data;
+    cin >> t;
+    while(t-- > 0){
+        cin >> data;
+        root = myTree.insert(root, data);
+    }
+    int height = myTree.getHeight(root);
+    cout << height;
+    return 0;
+}
