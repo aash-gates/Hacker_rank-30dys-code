91adb8cdd6688e0cef43150fe93596cf8300dd09
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-01 12:29:38 +0530

    Update day13.js

--- day13.js
+++ day13.js
@@ -1,46 +1,15 @@
-## Head Code ## 
-class Book
-    attr_accessor :title
-	attr_accessor :author
-	
-	def initialize(title, author)
-		raise 'You cannot instantiate an abstract class.'
-	end
-	
-	def display
-		raise 'You must override this method in your implementing class.'
-	end
-end
-
-class MyBook < Book
-	attr_accessor :price
-
-    ## Solution Code ##
-    #   Class Constructor
-	#   
-	#   Parameters:
-	#   title - The book's title.
-	#   author - The book's author.
-	#   price - The book's price.
-	def initialize(title, author, price)
-		@title = title
-		@author = author
-		@price = price
-	end
-	
-	#   Function Name: display
-	#   Print the title, author, and price in the specified format.
-	#
-	def display()
-	    print("Title: ", self.title, "Author: ", self.author, "Price: ", self.price)
-	end
-end
-
-## Tail Code ##
-
-title = gets
-author = gets
-price = gets
-
-new_novel = MyBook.new(title, author, price)
-new_novel.display    
+class MyBook extends Book {
+    
+    constructor(title, author, price) {
+        super(title, author)
+        this.price = price
+    }
+    
+    display() {
+        console.log(
+            'Title: ' + this.title + '\n' +
+            'Author: ' + this.author + '\n' +
+            'Price: ' + this.price
+        )
+    }
+}
