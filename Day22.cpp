d67ab338d2643dbec22fb50ce4aad48fbe96d4f4
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-01 17:25:38 +0530

    Update Day22.cpp

--- Day22.cpp
+++ Day22.cpp
@@ -1,58 +1,51 @@
-#include <iostream>
-#include <cstddef>
-using namespace std;    
+import java.util.*;
+import java.io.*;
 class Node{
-    public:
-        int data;
-        Node *left;
-        Node *right;
-        Node(int d){
-            data = d;
-            left = NULL;
-            right = NULL;
-        }
-};
-class Solution{
-    public:
-          Node* insert(Node* root, int data) {
-            if(root == NULL) {
-                return new Node(data);
-            }
-            else {
-                Node* cur;
-                if(data <= root->data){
-                    cur = insert(root->left, data);
-                    root->left = cur;
-                }
-                else{
-                    cur = insert(root->right, data);
-                    root->right = cur;
-               }
-               return root;
-           }
-        }
-        int getHeight(Node* root){
-          //Write your code here
-          if(!root) {
-              return -1;
-          }
-          int leftDepth = getHeight(root->left);
-          int rightDepth = getHeight(root->right);
-              
-          return (leftDepth > rightDepth ? leftDepth : rightDepth) + 1;
-        }
-}; //End of Solution
-int main() {
-    Solution myTree;
-    Node* root = NULL;
-    int t;
+    Node left,right;
     int data;
-    cin >> t;
-    while(t-- > 0){
-        cin >> data;
-        root = myTree.insert(root, data);
+    Node(int data){
+        this.data=data;
+        left=right=null;
     }
-    int height = myTree.getHeight(root);
-    cout << height;
-    return 0;
+}
+class Solution{
+ public static int getHeight(Node root){
+      int heightleft=0;
+      int heightright=0;
+      if(root.left!=null){
+          heightleft=getHeight(root.left)+1;
+      }
+      if(root.right!=null){
+          heightright=getHeight(root.right)+1;
+      }
+      return (heightleft>heightright?heightleft:heightright);
+    }
+    public static Node insert(Node root,int data){
+        if(root==null){
+            return new Node(data);
+        }
+        else{
+            Node cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
+  public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        Node root=null;
+        while(T-->0){
+            int data=sc.nextInt();
+            root=insert(root,data);
+        }
+        int height=getHeight(root);
+        System.out.println(height);
+    }
 }
