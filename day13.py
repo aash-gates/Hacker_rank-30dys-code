e6e7cffd4b42b1f8ab340cf235cf415b24eab832
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-01 12:37:18 +0530

    cg

--- day13.py
+++ day13.py
@@ -1,15 +1,18 @@
-class MyBook extends Book {
-    
-    constructor(title, author, price) {
-        super(title, author)
-        this.price = price
-    }
-    
-    display() {
-        console.log(
-            'Title: ' + this.title + '\n' +
-            'Author: ' + this.author + '\n' +
-            'Price: ' + this.price
-        )
-    }
-}
+from abc import ABCMeta, abstractmethod
+class Book(object, metaclass=ABCMeta):
+    def __init__(self,title,author):
+        self.title=title
+        self.author=author   
+    @abstractmethod
+    def display(): pass
+class MyBook(Book):
+    def __init__(self, title, author, price):
+        super().__init__(title, author)
+        self.price = price
+    def display(self):
+        print("Title: " + self.title + "\nAuthor: " + self.author + "\nPrice: " + str(self.price))
+title=input()
+author=input()
+price=int(input())
+new_novel=MyBook(title,author,price)
+new_novel.display()
--- day14.java
+++ day14.java
@@ -1,18 +1,58 @@
-import Foundation
+import java.io.*;
+import java.util.*;
 
-class Difference {
-    private var elements = [Int]()
-    var maximumDifference: Int
-	 init(a : [Int]) {
-        self.elements = a
-        self.maximumDifference = 0
+class Node {
+	int data;
+	Node next;
+	Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class Solution {
+
+   public static  Node insert(Node head,int data) {
+    // This is a "method-only" submission. 
+    // You only need to complete this method. 
+    Node current = head;
+    // Setting up our new node to add
+    Node newTail = new Node(data);
+    newTail.next = null;
+    // if the list isn't empty
+    if (current != null) {
+        // get to the end of the list and set the current
+        // to the last element
+       while (current.next != null) {
+           // iterating through
+            current = current.next;
+        }
+    } else {
+        // otherwise the list is empty and we should just return our new node with the data
+        return newTail;
+    }
+    // then add our new tail onto the end of the list
+    current.next = newTail;
+    return head;
+}
+	public static void display(Node head) {
+        Node start = head;
+        while(start != null) {
+            System.out.print(start.data + " ");
+            start = start.next;
+        }
     }
 
-    /** Sorts the array
-    *   Saves max abs difference between first and last element to maximumDifference. **/
-    func computeDifference() {
-        self.elements = self.elements.sorted()
-        self.maximumDifference = elements[elements.count - 1] - elements[0]
-    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+        int N = sc.nextInt();
 
-} // End of Difference class
+        while(N-- > 0) {
+            int ele = sc.nextInt();
+            head = insert(head,ele);
+        }
+        display(head);
+        sc.close();
+    }
+}
